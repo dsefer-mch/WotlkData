b0dca6789ca68f380ec55455185e9a2e294cd875
mch <mch.dv.uk@gmai.com>
2022-02-04 20:22:52 +0000

    Adds chromebrowserdriver to the repository, update the bot

--- wotlk_craper_bot.py
+++ wotlk_craper_bot.py
@@ -1,26 +1,40 @@
 from selenium import webdriver
-import json
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options 
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver import ActionChains
-from selenium.common.exceptions import TimeoutException, WebDriverException
-from time import sleep, time
 import requests
 
 
-class Bot():
-    def __init__(self):
+class ScriperBot():
+    def __init__(self, verbose=False):
         print('Bot initialising...')
         self.driver = webdriver.Chrome()
         self.driver.maximize_window()
         self.action = ActionChains(self.driver)
-        url = "https://wotlkdb.com/"
+        self.url = "https://wotlkdb.com/"
+        self.verbose = verbose
+
+    def open_chrome_url(self, headless=False):
+        timeout = 5
+        try:
+            requests.get(self.url, timeout=timeout)
+        except (requests.ConnectionError, requests.Timeout) as exception:
+            print("No internet connection.")
+            return self.driver.quit()
+        if headless:
+            chrome_options = Options()
+            chrome_options.add_argument("headless")
+            self.driver = webdriver.Chrome(options=chrome_options)
+        self.driver.get(self.url)
+        self.driver.implicitly_wait(10)
 
     def find_element(self, element, type='textlink'):
         try:
+            if type == 'id':
+                obect = self.driver.find_element(By.ID, element)
+                return obect
             if type == 'textlink':
                 obect = self.driver.find_element(By.LINK_TEXT, element)
                 return obect
@@ -38,7 +52,7 @@ class Bot():
 
     def move_cursor(self, obect):
         try:
-           self.action.move_to_element(obect).perform()
+            self.action.move_to_element(obect).perform()
 
         except:
             EC.NoSuchElementException(f'Object not found...')
@@ -47,35 +61,41 @@ class Bot():
         try:
             self.clear()
         except:
-            EC.NoSuchElementException('Field to clear not found or not a propper type...')
+            EC.NoSuchElementException(
+                'Field to clear not found or not a propper type...')
 
-    def set_filter(self, value):
+    def set_field(self, value):
         try:
             self.send_keys(value)
         except:
-            EC.NoSuchElementException('Field to clear not found or not a propper type...')
+            EC.NoSuchElementException(
+                'Field to clear not found or not a propper type...')
 
     def hit_enter(self):
         try:
             self.send_keys(Keys.RETURN)
         except:
-            EC.NoSuchElementException('Field to clear not found or not a propper type...')
+            EC.NoSuchElementException(
+                'Field to clear not found or not a propper type...')
 
-        
     def move_cursor(self):
         try:
-           self.action.move_to_element(self).perform()
+            self.action.move_to_element(self).perform()
 
         except:
             EC.NoSuchElementException(f'Object not found...')
 
+    def driver_quit(self):
+        self.driver.quit()
 
 
+    # def download_file(self, src_url, local_destination):
+    #     response = requests.get(src_url)
+    #     with open(local_destination, 'wb+') as f:
+    #         f.write(response.content)
 
-    def download_file(self, src_url, local_destination):
-        response = requests.get(src_url)
-        with open(local_destination, 'wb+') as f:
-            f.write(response.content)
 
 if __name__ == '__main__':
-    # EXAMPLE USAGE
+    bot = ScriperBot(verbose=True)
+    bot.open_chrome_url()
+    bot.driver_quit()
