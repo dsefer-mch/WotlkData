636514f1fd0c63a4c8d9d8fde41045a8c32f5a12
mch <mch.dv.uk@gmai.com>
2022-02-10 17:28:45 +0000

    Modifies both .py -files .partualy working scraper :)

--- wotlk_scraper.py
+++ wotlk_scraper.py
@@ -1,6 +1,123 @@
-        self.num_of_items = num_of_items
-    def num_of_items_on_the_page(self):
-        try:
-            num_of_items = int(self.driver.find_element_by_xpath('//*[@class="listview-note"]').text.split()[0])
-        except:
-             EC.NoSuchElementException('Number of items cannot be determined.')
+from tabnanny import verbose
+from wotlk_scraper_bot import ScraperBot
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+import pandas as pd
+import re
+
+
+class Scraper(ScraperBot):
+
+    weapons = ['Daggers', 'Fist Weapons', 'One-Handed Axes', 'One-Handed Maces', 'One-Handed Swords', 'Polearms', 'Staves',
+               'Two-Handed Axes', 'Two-Handed Maces', 'Two-Handed Swords', 'Bows', 'Crossbows', 'Guns', 'Thrown', 'Wands']
+    armor = ['Chest', 'Feet', 'Hands', 'Head', 'Legs', 'Shoulder', 'Wrist', 'Waist', 'Cloaks',
+             'Shields', 'Amulets', 'Rings', 'Trinkets', 'Idols', 'Librams', 'Totems', 'Sigils']
+    armor_type = ['Cloth', 'Leather', 'Mail', 'Plate']
+    armor_08 = ['Chest', 'Feet', 'Hands', 'Head',
+                'Legs', 'Shoulder', 'Wrist', 'Waist']
+
+    def __init__(self, item, arm_type='Plate'):
+
+        super().__init__()
+        self.item = item
+        self.arm_type = arm_type
+        self.list_of_items = []
+
+    def nav_to_main_page(self):
+        action = ActionChains(self.driver)
+        database = self.driver.find_element(By.LINK_TEXT, 'Database')
+        action.move_to_element(database)
+        action.perform()
+        items = self.driver.find_element(
+            By.XPATH, "/html/body/div[7]/div/div/a[4]/span")
+        action.move_to_element(items)
+        action.perform()
+        if self.item in Scraper.weapons:
+            weapons = self.driver.find_element(By.LINK_TEXT, 'Weapons')
+            action.move_to_element(weapons)
+            action.perform()
+            weapon = self.driver.find_element(By.LINK_TEXT, self.item).click()
+            action.perform()
+            try:
+                filter_button = self.driver.find_element(By.XPATH, '//*[@id="fi_toggle"]').text
+                print('FILTER ON/OFF?', filter_button)
+                if filter_button == 'Create a filter':
+                    button = self.driver.find_element(By.XPATH, '//*[@id="fi_toggle"]').click()
+                action.perform()
+            except:
+                EC.NoSuchElementException
+        else:
+            armor = self.driver.find_element(By.LINK_TEXT, 'Armor')
+            action.move_to_element(armor)
+            action.perform()
+            if self.item in Scraper.armor_08:
+                armor_type_menu = self.driver.find_element(
+                    By.LINK_TEXT, self.arm_type)
+                action.move_to_element(armor_type_menu)
+                action.perform()
+                armor_item = self.driver.find_element(
+                    By.LINK_TEXT, self.item).click()
+                action.perform()
+                try:
+                    filter_button = self.driver.find_element(By.XPATH, '//*[@id="fi_toggle"]').text
+                    print('FILTER ON/OFF?', filter_button)
+                    if filter_button == 'Create a filter':
+                        button = self.driver.find_element(By.XPATH, '//*[@id="fi_toggle"]').click()
+                    action.perform()
+                except:
+                    EC.NoSuchElementException
+
+    def use_web_filter(self):
+        min_item_lvl = 1
+        max_item_lvl = 20    # Change it for less or more scraping 10 -290
+        step = 10
+        bott_ilvl = 0
+        top_ilvl = 9
+        for ilvl_set in [(bott_ilvl + i, top_ilvl + i) for i in range(min_item_lvl, max_item_lvl, step)]:
+            min_lvl = self.driver.find_element(By.NAME, 'minle')
+            min_lvl.clear()
+            min_lvl.send_keys(ilvl_set[0])
+            max_lvl = self.driver.find_element(By.NAME, 'maxle')
+            max_lvl.clear()
+            max_lvl.send_keys(ilvl_set[1])
+            max_lvl.send_keys(Keys.RETURN)
+            self.driver.implicitly_wait(10)
+            self.scrape_item_nums()
+
+    def scrape_item_nums(self):
+        all_items_list = re.findall(r'(\w+)=(\d+)', self.driver.page_source)
+        all_items_list_as_a_list = [
+            list(element_in_the_item_list) for element_in_the_item_list in all_items_list]
+        list_of_real_items = []
+        [list_of_real_items.append(element) for element in all_items_list_as_a_list if element[0]
+         == 'item' if element not in list_of_real_items]
+        self.list_of_items += list_of_real_items
+
+    def scrape_the_items(self):
+        df = pd.DataFrame({})
+        for i in self.list_of_items:
+            if verbose == True:
+                print('this is just the element i :', i)
+            self.driver.get("https://wotlkdb.com/?item=" + i[1])
+            scraped_data = self.driver.find_element(
+                By.XPATH, "(//TD)[4]/../../../..").text
+            libr = {
+                'Item': scraped_data
+            }
+            df = df.append(libr, ignore_index=True)
+        df.to_csv('data_test.csv')
+
+
+if __name__ == '__main__':
+
+    s = Scraper('Waist', 'Mail')
+    s.verbose = True
+    s.accept_cookies()
+    s.nav_to_main_page()
+    s.use_web_filter()
+    s.scrape_item_nums()
+    s.scrape_the_items()
+    s.delete_cookies()
+    s.driver.quit()
--- wotlk_scraper_bot.py
+++ wotlk_scraper_bot.py
@@ -1,146 +1,61 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import ActionChains
 import requests
 from time import sleep
+import subprocess, os
 
 
-class ScriperBot():
-    def __init__(self, verbose=False):
+def open_chrome(port=9200, on_mac=False):
+    my_env = os.environ.copy()
+    if on_mac:
+        print('opening chrome')
+        subprocess.Popen(['open', '-a', "Google Chrome", '--args', f'--remote-debugging-port={port}', 'http://www.example.com'], env=my_env)
+    else:
+        subprocess.Popen(f'google-chrome --remote-debugging-port={port} --user-data-dir=data_dir'.split(), env=my_env)
+    print('opened chrome')
+
+class ScraperBot():
+
+    def __init__(self, verbose=False, headless=False):
         print('Bot initialising...')
         self.driver = webdriver.Chrome()
         self.driver.maximize_window()
-        self.action = ActionChains(self.driver)
         self.url = "https://wotlkdb.com/"
+        self.driver.get(self.url)
+        self.driver.implicitly_wait(10)
         self.verbose = verbose
-
-    def open_chrome_url(self, headless=False):
-        timeout = 5
-        try:
-            requests.get(self.url, timeout=timeout)
-        except (requests.ConnectionError, requests.Timeout) as exception:
-            print("No internet connection.")
-            return self.driver.quit()
         if headless:
             chrome_options = Options()
             chrome_options.add_argument("headless")
+            chrome_options.add_experimental_option(f"debuggerAddress", f"127.0.0.1:9200")
             self.driver = webdriver.Chrome(options=chrome_options)
-        self.driver.get(self.url)
-        self.driver.implicitly_wait(10)
+        options = Options()
+        options.add_argument('--no-sandbox')
+
 
     def accept_cookies(self):
-        print('1')
+        action = ActionChains(self.driver)
         try:
-            print('2')
-            butt = self.find_element_by_element('Accept')
-            print('1.1', butt)
-            if butt == None:
-                print('here we are in the xpath br')
-                butt = self.find_element_by_element(
-                    '//*[@id="ncmp__tool"]/div/div/div[3]/div[1]/button[2]', 'xpath')
-            print(butt)
-            self.move_cursor(butt)
-            print('4')
-            print(butt)
-            self.click_on(butt)
+            accept_button = self.driver.find_element(By.XPATH, '//*[@id="ncmp__tool"]/div/div/div[3]/div[1]/button[2]')
+            action.move_to_element(accept_button)
+            action.perform()
+            action.click()
+            action.perform()
         except:
             EC.NoSuchElementException(f'Something went wrong or NO cookies...')
 
     def delete_cookies(self):
         self.driver.delete_all_cookies()
-
-    def find_element_by_element(self, element, type='textlink'):
-        try:
-            print('6')
-            if type == 'id':
-                print('its in the id')
-                obj = self.driver.find_element(By.ID, element)
-                print('thats after the oject')
-                print('what the fack?', obj)
-                return obj
-            elif type == 'xpath':
-                obj = self.driver.find_element(By.XPATH, element)
-                return obj
-            elif type == 'textlink':
-                print('7')
-                print(element)
-                sleep(10)
-                obj = self.driver.find_element(By.LINK_TEXT, element)
-                print('8')
-                print(obj)
-                return obj
-            elif type == 'name':
-                obj = self.driver.find_element(By.NAME, element)
-                return obj
-            elif type == 'tag':
-                obj = self.driver.find_element(By.TAG_NAME, element)
-                return obj
-            elif type == 'class':
-                obj = self.driver.find_element(By.CLASS_NAME, element)
-                return obj
-        except:
-            EC.NoSuchElementException('No such element found...')
-
-    def move_cursor(self, obj):
-        try:
-            print('9')
-            print(obj)
-            self.action.move_to_element(obj)
-            self.action.perform()
-            return
-        except:
-            EC.NoSuchElementException(f'Object not found...')
-
-    def click_on(self, obj):
-        try:
-            print('10')
-            print(obj)
-            self.action.click(obj)
-            self.action.perform()
-        except:
-            EC.NoSuchElementException(f'Nothing to click on...')
-
-    def clear_field(self):
-        try:
-            self.clear()
-            return
-        except:
-            EC.NoSuchElementException(
-                'Field to clear not found or not a propper type...')
-
-    def set_field(self, value):
-        try:
-            self.send_keys(value)
-            return
-        except:
-            EC.NoSuchElementException(
-                'Field to clear not found or not a propper type...')
-
-    def hit_enter(self):
-        try:
-            return self.send_keys(Keys.RETURN)
-        except:
-            EC.NoSuchElementException(
-                'Field to clear not found or not a propper type...')
-
-    def driver_quit(self):
-        self.driver.quit()
-
-    # def download_file(self, src_url, local_destination):
-    #     response = requests.get(src_url)
-    #     with open(local_destination, 'wb+') as f:
-    #         f.write(response.content)
+        print('does it cleans it?')
 
 
 if __name__ == '__main__':
-    bot = ScriperBot(verbose=True)
-    bot.open_chrome_url()
+    bot = ScraperBot()
+    # bot.open_chrome_url(headless=True)
     bot.accept_cookies()
-    found = bot.find_element_by_element('Database')
-    bot.move_cursor(found)
-    sleep(20)
     bot.delete_cookies()
-    bot.driver_quit()
+    sleep(10)
+    bot.driver.quit()
